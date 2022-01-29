eaa31d1621b0e2cfdfd21c86b8475f2b9c49b8f9
Ahmed Agha <ahmed.agha@tum.de>
2022-01-29 21:33:51 +0100

    add def main

--- main_sac.py
+++ main_sac.py
@@ -12,144 +12,144 @@ from models import gail
 import utils.utils
 
 
-"""def main(config=None, args_dict=None, overwrite=False):
-if args_dict is None:"""
-args_dict = get_args_dict(config="configs/config_sac.yaml")
+def main(config=None, args_dict=None, overwrite=False):
+    if args_dict is None:
+        args_dict = get_args_dict(config=config)
 
-# Environment
-# env = NormalizedActions(gym.make(args.env_name))
-env = gym.make(args_dict['env_name'])
-env.seed(args_dict['seed'])
-env.action_space.seed(args_dict['seed'])
+    # Environment
+    # env = NormalizedActions(gym.make(args.env_name))
+    env = gym.make(args_dict['env_name'])
+    env.seed(args_dict['seed'])
+    env.action_space.seed(args_dict['seed'])
 
-torch.manual_seed(args_dict['seed'])
-np.random.seed(args_dict['seed'])
+    torch.manual_seed(args_dict['seed'])
+    np.random.seed(args_dict['seed'])
 
-# Agent
-agent = SAC(env.observation_space.shape[0], env.action_space, args_dict)
+    # Agent
+    agent = SAC(env.observation_space.shape[0], env.action_space, args_dict)
 
-# Tesnorboard
-writer = SummaryWriter(
-    'runs/{}_SAC_{}_{}_{}'.format(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"), args_dict['env_name'],
-                                  "Gaussian", "autotune" if args_dict['automatic_entropy_tuning'] else ""))
+    # Tesnorboard
+    writer = SummaryWriter(
+        'runs/{}_SAC_{}_{}_{}'.format(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"), args_dict['env_name'],
+                                      "Gaussian", "autotune" if args_dict['automatic_entropy_tuning'] else ""))
 
-# Memory
-memory = ReplayMemory(1000000, args_dict['seed'])
+    # Memory
+    memory = ReplayMemory(1000000, args_dict['seed'])
 
-# Training Loop
-total_numsteps = 0
-updates = 0
-device = 'cpu'
+    # Training Loop
+    total_numsteps = 0
+    updates = 0
+    device = 'cpu'
 
-discr = None
-gail_train_loader = None
-if args_dict['use_gail']:
-    file_name = args_dict['gail_experts_dir'] + args_dict['env_name'] + \
-                '_num_traj_' + str(args_dict['num_trajectories']) + '.pt'
-    subsample_frequency = None
-    if args_dict['env_name'] == "Reacher-v2":
-        subsample_frequency = 1
-    elif args_dict['env_name'] == "door-v0" or args_dict['env_name'] == "hammer-v0" or args_dict[
-        'env_name'] == "relocate-v0":
-        subsample_frequency = 4
-    elif args_dict['env_name'] == "pen-v0":
-        subsample_frequency = 2
-    else:
-        subsample_frequency = 20
-    expert_dataset = gail.ExpertDataset(
-        file_name, num_trajectories=args_dict['num_trajectories'], subsample_frequency=subsample_frequency)
-    assert len(env.observation_space.shape) == 1
-    discr = gail.Discriminator(
-        input_dim=(env.observation_space.shape[0] + env.action_space.shape[0]),
-        hidden_dim=100, device=device, gradient_penalty=args_dict['gradient_penalty'], lr_disc=args_dict['lr_disc'],
-        spectral_norm=args_dict['spectral_norm'],
-        airl_reward=args_dict['airl_reward'])
-    drop_last = len(expert_dataset) > args_dict['gail_batch_size']
-    gail_train_loader = torch.utils.data.DataLoader(
-        dataset=expert_dataset,
-        batch_size=args_dict['gail_batch_size'],
-        shuffle=True,
-        drop_last=drop_last)
-
-for i_episode in itertools.count(1):
-    episode_reward = 0
-    episode_steps = 0
-    done = False
-    state = env.reset()
-
-    while not done:
-        if args_dict['start_steps'] > total_numsteps:
-            action = env.action_space.sample()  # Sample random action
+    discr = None
+    gail_train_loader = None
+    if args_dict['use_gail']:
+        file_name = args_dict['gail_experts_dir'] + args_dict['env_name'] + \
+                    '_num_traj_' + str(args_dict['num_trajectories']) + '.pt'
+        subsample_frequency = None
+        if args_dict['env_name'] == "Reacher-v2":
+            subsample_frequency = 1
+        elif args_dict['env_name'] == "door-v0" or args_dict['env_name'] == "hammer-v0" or args_dict[
+            'env_name'] == "relocate-v0":
+            subsample_frequency = 4
+        elif args_dict['env_name'] == "pen-v0":
+            subsample_frequency = 2
         else:
-            action = agent.select_action(state)  # Sample action from policy
+            subsample_frequency = 20
+        expert_dataset = gail.ExpertDataset(
+            file_name, num_trajectories=args_dict['num_trajectories'], subsample_frequency=subsample_frequency)
+        assert len(env.observation_space.shape) == 1
+        discr = gail.Discriminator(
+            input_dim=(env.observation_space.shape[0] + env.action_space.shape[0]),
+            hidden_dim=100, device=device, gradient_penalty=args_dict['gradient_penalty'], lr_disc=args_dict['lr_disc'],
+            spectral_norm=args_dict['spectral_norm'],
+            airl_reward=args_dict['airl_reward'])
+        drop_last = len(expert_dataset) > args_dict['gail_batch_size']
+        gail_train_loader = torch.utils.data.DataLoader(
+            dataset=expert_dataset,
+            batch_size=args_dict['gail_batch_size'],
+            shuffle=True,
+            drop_last=drop_last)
 
-        if len(memory) > args_dict['mini_batch_size']:
-            if args_dict['use_gail']:
-                for _ in range(args_dict['updates_per_step']):
-                    # expert_batch = expert_dataset.sample_batch(batch_size=args_dict['gail_batch_size'])
-                    _, disc_grad_norm_epoch, acc_policy_epoch, acc_expert_epoch = \
-                        discr.update_sac(gail_train_loader, memory)
-            # Number of updates per step in environment
-            for i in range(args_dict['updates_per_step']):
-                # Update parameters of all the networks
-                critic_1_loss, critic_2_loss, policy_loss, ent_loss, alpha = agent.update_parameters(memory,
-                                                                                                     args_dict[
-                                                                                                         'mini_batch_size'],
-                                                                                                     updates,
-                                                                                                     discr=discr)
+    for i_episode in itertools.count(1):
+        episode_reward = 0
+        episode_steps = 0
+        done = False
+        state = env.reset()
 
-                writer.add_scalar('loss/critic_1', critic_1_loss, updates)
-                writer.add_scalar('loss/critic_2', critic_2_loss, updates)
-                writer.add_scalar('loss/policy', policy_loss, updates)
-                writer.add_scalar('loss/entropy_loss', ent_loss, updates)
-                writer.add_scalar('entropy_temprature/alpha', alpha, updates)
-                updates += 1
+        while not done:
+            if args_dict['start_steps'] > total_numsteps:
+                action = env.action_space.sample()  # Sample random action
+            else:
+                action = agent.select_action(state)  # Sample action from policy
 
-        next_state, reward, done, _ = env.step(action)  # Step
-        episode_steps += 1
-        total_numsteps += 1
-        episode_reward += reward
+            if len(memory) > args_dict['mini_batch_size']:
+                if args_dict['use_gail']:
+                    for _ in range(args_dict['updates_per_step']):
+                        # expert_batch = expert_dataset.sample_batch(batch_size=args_dict['gail_batch_size'])
+                        _, disc_grad_norm_epoch, acc_policy_epoch, acc_expert_epoch = \
+                            discr.update_sac(gail_train_loader, memory)
+                # Number of updates per step in environment
+                for i in range(args_dict['updates_per_step']):
+                    # Update parameters of all the networks
+                    critic_1_loss, critic_2_loss, policy_loss, ent_loss, alpha = agent.update_parameters(memory,
+                                                                                                         args_dict[
+                                                                                                             'mini_batch_size'],
+                                                                                                         updates,
+                                                                                                         discr=discr)
 
-        # Ignore the "done" signal if it comes from hitting the time horizon.
-        # (https://github.com/openai/spinningup/blob/master/spinup/algos/sac/sac.py)
-        mask = 1 if episode_steps == env._max_episode_steps else float(not done)
+                    writer.add_scalar('loss/critic_1', critic_1_loss, updates)
+                    writer.add_scalar('loss/critic_2', critic_2_loss, updates)
+                    writer.add_scalar('loss/policy', policy_loss, updates)
+                    writer.add_scalar('loss/entropy_loss', ent_loss, updates)
+                    writer.add_scalar('entropy_temprature/alpha', alpha, updates)
+                    updates += 1
 
-        memory.push(state, action, reward, next_state, mask)  # Append transition to memory
+            next_state, reward, done, _ = env.step(action)  # Step
+            episode_steps += 1
+            total_numsteps += 1
+            episode_reward += reward
 
-        state = next_state
+            # Ignore the "done" signal if it comes from hitting the time horizon.
+            # (https://github.com/openai/spinningup/blob/master/spinup/algos/sac/sac.py)
+            mask = 1 if episode_steps == env._max_episode_steps else float(not done)
 
-    if total_numsteps > args_dict['num_env_steps']:
-        break
+            memory.push(state, action, reward, next_state, mask)  # Append transition to memory
 
-    writer.add_scalar('reward/train', episode_reward, i_episode)
-    print("Episode: {}, total numsteps: {}, episode steps: {}, reward: {}".format(i_episode, total_numsteps,
-                                                                                  episode_steps,
-                                                                                  round(episode_reward, 2)))
+            state = next_state
 
-    if i_episode % 10 == 0:
-        avg_reward = 0.
-        episodes = 10
-        for _ in range(episodes):
-            state = env.reset()
-            episode_reward = 0
-            done = False
-            while not done:
-                action = agent.select_action(state, evaluate=True)
+        if total_numsteps > args_dict['num_env_steps']:
+            break
 
-                next_state, reward, done, _ = env.step(action)
-                episode_reward += reward
+        writer.add_scalar('reward/train', episode_reward, i_episode)
+        print("Episode: {}, total numsteps: {}, episode steps: {}, reward: {}".format(i_episode, total_numsteps,
+                                                                                      episode_steps,
+                                                                                      round(episode_reward, 2)))
 
-                state = next_state
-            avg_reward += episode_reward
-        avg_reward /= episodes
+        if i_episode % 10 == 0:
+            avg_reward = 0.
+            episodes = 10
+            for _ in range(episodes):
+                state = env.reset()
+                episode_reward = 0
+                done = False
+                while not done:
+                    action = agent.select_action(state, evaluate=True)
 
-        writer.add_scalar('avg_reward/test', avg_reward, i_episode)
+                    next_state, reward, done, _ = env.step(action)
+                    episode_reward += reward
 
-        print("----------------------------------------")
-        print("Test Episodes: {}, Avg. Reward: {}".format(episodes, round(avg_reward, 2)))
-        print("----------------------------------------")
+                    state = next_state
+                avg_reward += episode_reward
+            avg_reward /= episodes
 
-env.close()
+            writer.add_scalar('avg_reward/test', avg_reward, i_episode)
+
+            print("----------------------------------------")
+            print("Test Episodes: {}, Avg. Reward: {}".format(episodes, round(avg_reward, 2)))
+            print("----------------------------------------")
+
+    env.close()
 
 
 if __name__ == "__main__":
