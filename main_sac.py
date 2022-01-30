c47620345312aa981e8a7c456c6362e51a1fc555
Ahmed Agha <ahmed.agha@tum.de>
2022-01-30 18:59:10 +0100

    add logging and model saving

--- main_sac.py
+++ main_sac.py
@@ -10,11 +10,44 @@ from models.replay_buffer import ReplayMemory
 from utils.arguments import get_args_dict
 from models import gail
 import utils.utils
+import os
+import shutil
+import yaml
+import csv
+from collections import deque
 
 
 def main(config=None, args_dict=None, overwrite=False):
     if args_dict is None:
         args_dict = get_args_dict(config=config)
+
+    exp_name = "logs_gail_sac/"
+    log_dir_ = args_dict['logging_dir'] + args_dict['env_name'] + '/' + exp_name
+
+    f = None
+    if args_dict['logging']:
+        if os.path.isdir(log_dir_):
+            if overwrite:
+                shutil.rmtree(log_dir_)
+            else:
+                print("experiment already exists !")
+                return
+
+        os.makedirs(log_dir_)
+        os.makedirs(log_dir_ + '/summary')
+        os.makedirs(log_dir_ + '/logs')
+        os.makedirs(log_dir_ + '/models')
+
+        f = open(log_dir_ + '/logs/log.csv', 'w')
+
+    fnames = ['total_num_steps', 'mean_training_episode_reward', 'mean_eval_episode_rewards']
+    csv_writer = None
+    if args_dict['logging']:
+        csv_writer = csv.DictWriter(f, fieldnames=fnames)
+        csv_writer.writeheader()
+
+        with open(log_dir_ + '/args.yml', 'w') as outfile:
+            yaml.dump(args_dict, outfile, default_flow_style=False)
 
     # Environment
     # env = NormalizedActions(gym.make(args.env_name))
@@ -71,6 +104,8 @@ def main(config=None, args_dict=None, overwrite=False):
             shuffle=True,
             drop_last=drop_last)
 
+    episode_rewards = deque(maxlen=5)
+    best_eval = -np.inf
     for i_episode in itertools.count(1):
         episode_reward = 0
         episode_steps = 0
@@ -122,9 +157,10 @@ def main(config=None, args_dict=None, overwrite=False):
             break
 
         writer.add_scalar('reward/train', episode_reward, i_episode)
+        episode_rewards.append(episode_reward)
         print("Episode: {}, total numsteps: {}, episode steps: {}, reward: {}".format(i_episode, total_numsteps,
                                                                                       episode_steps,
-                                                                                      round(episode_reward, 2)))
+                                                                                      np.mean(episode_rewards), 2))
 
         if i_episode % 10 == 0:
             avg_reward = 0.
@@ -148,6 +184,19 @@ def main(config=None, args_dict=None, overwrite=False):
             print("----------------------------------------")
             print("Test Episodes: {}, Avg. Reward: {}".format(episodes, round(avg_reward, 2)))
             print("----------------------------------------")
+
+            if args_dict['save_model'] and avg_reward >= best_eval:
+                torch.save([
+                    agent.policy,
+                    getattr(utils.utils.get_vec_normalize(env), 'obs_rms', None)
+                ], os.path.join(log_dir_ + '/models', args_dict['env_name'] + ".pt"))
+                best_eval = avg_reward
+
+            if args_dict['logging']:
+                csv_writer.writerow({'total_num_steps': total_numsteps,
+                                     'mean_training_episode_reward': np.mean(episode_rewards),
+                                     'mean_eval_episode_rewards': avg_reward})
+                f.flush()
 
     env.close()
 
