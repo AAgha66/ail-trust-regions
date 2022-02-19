ca35bb2b3d17786d1bdebdc8d2d12a83f4f2da7c
Ahmed Agha <ahmed.agha@tum.de>
2022-02-19 13:42:20 +0100

    removing old stuff from argument loading

--- utils/arguments.py
+++ utils/arguments.py
@@ -4,7 +4,4 @@ import yaml
 def get_args_dict(config):
     with open(config) as info:
         args_dict = yaml.safe_load(info)
-        assert args_dict['algo'] in ['ppo', 'trpo']
-        if args_dict['eval_interval'] == 'None':
-            args_dict['eval_interval'] = None
     return args_dict
